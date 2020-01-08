724c945750dcd1d9189d6791354a8648f9755e30
James <mail7788897887@gmail.com>
2020-01-08 03:22:01 +0000

    log head of an array

---
+++ head.js
@@ -0,0 +1,15 @@
+//const newFunction = function() {
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log("🔥🔥🔥 Assertion Passed: " + actual + " === " + expected);
+    } else {
+      console.log("💩💩💩 Assertion Failed: " + actual + " !== " + expected);
+    }
+  };
+  const head = function(array) {
+    return array[0];
+  };
+  assertEqual(head([5,6,7]), 5);
+  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  
+
