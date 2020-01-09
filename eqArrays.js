8c3baccff81c29a72e4e0a70493074ba6dda3c6c
James <mail7788897887@gmail.com>
2020-01-09 03:28:29 +0000

    equal arrays

--- eqArrays.js
+++ eqArrays.js
@@ -20,8 +20,9 @@ const eqArrays = function(arr1, arr2) {
    return true;
 }
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [3, 2, 1]);
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
