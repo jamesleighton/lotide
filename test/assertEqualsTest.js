23c7c7be42710e111f2093ec32ffbd25006c2005
James <mail7788897887@gmail.com>
2020-01-13 22:22:16 +0000

    assert equal test and head test files

---
+++ test/assertEqualsTest.js
@@ -0,0 +1,18 @@
+const assertEqual = require('../assertEqual');
+
+//const newFunction = function() {
+  //const assertEqual = function(actual, expected) {
+   // if (actual === expected) {
+   //   console.log(`🔥🔥🔥 Assertion Passed: ${actual} === ${expected}`);
+   // } else {
+   //   console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+   // }
+  //};
+  //assertEqual("Lighthouse Labs", "Bootcamp");
+  //assertEqual(1, 1);
+  
+//};
+//newFunction();
+
+
+module.exports = assertEqual;
