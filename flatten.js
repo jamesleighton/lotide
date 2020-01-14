88eb90ca6afd81efa9bbe43fc6f603e36024486b
James <mail7788897887@gmail.com>
2020-01-14 03:17:54 +0000

    flatten array

---
+++ flatten.js
@@ -0,0 +1,16 @@
+
+const data = [1, 2, [3, 4], 5, [6]];
+
+const flatten = function(arr, result = []) {
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i];
+    if (Array.isArray(value)) {
+      flatten(value, result);
+    } else {
+      result.push(value);
+    }
+  }
+  return result;
+};
+
+console.log(flatten(data));
